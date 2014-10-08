be8b7faf59b03240279055120a418a997ea7f7e9
Pat Sabpisal <ssabpisa@me.com>
2014-10-08 04:24:42 -0400

    More tools

--- tools/make_responses.js
+++ tools/make_responses.js
@@ -1,9 +1,29 @@
 #!/usr/bin/node
+/*
+# Copyright 2014 Open Ag Data Alliance
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+This scripts generate the response document from csv can data
+*/
+
 fs = require('fs');
 if(process.argv.length < 4){
   console.log("Usage: make_responses.js <data_file.csv> <template_name.json>");
   return;
 }
+
 var file = process.argv[2];
 var template = process.argv[3];
 var header = {}; //this is what a 'record' look like (will be generated)
